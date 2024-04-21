34ad923e838ae1574886635f777166d205208d8c
gzj198203 <155219828+gzj198203@users.noreply.github.com>
2024-04-21 17:35:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ceda2861-a4c6-464e-a8ff-df6df751746b';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
